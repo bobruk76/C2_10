a9f88790fde4dd2129129be2e995fc7704572708
Vladimir Poliakov <60501887+bobruk76@users.noreply.github.com>
2020-05-07 21:44:59 +0300

    begin!

---
+++ src/script.js
@@ -0,0 +1,48 @@
+
+Array.prototype.max = function () {
+  return Math.max.apply(Math, this);
+}
+
+
+const progressCats = document.querySelector('#progressbar-cats')
+const progressParrots = document.querySelector('#progressbar-parrots')
+const progressDogs = document.querySelector('#progressbar-dogs')
+
+const header = new Headers({
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Origin': '*'
+})
+
+const url = new URL('https://sf-pyw.mosyag.in/sse/vote/stats')
+const ES = new EventSource(url, header)
+
+function setProgressWidth(obj,value,maxValue){
+    widthProgress = Math.round(100*value/maxValue)
+    obj.style.cssText = `width: ${widthProgress}%`
+    obj.textContent = `${value}`
+
+}
+
+
+function init() {
+  ES.onerror = error => {
+    ES.readyState ? progress.textContent = "Some error" : null;
+  }
+
+  ES.onmessage = ({ data }) => {
+
+    // {"cats": 3915, "parrots": 6328, "dogs": 5306}
+    let result = JSON.parse(data);
+    let maxVotes = Object.values(result).max();
+
+    setProgressWidth(progressCats, result.cats, maxVotes);
+    setProgressWidth(progressParrots, result.parrots, maxVotes);
+    setProgressWidth(progressDogs, result.dogs, maxVotes);
+
+  }
+
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  init()
+});
